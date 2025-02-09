2c6bfff1f5f687d74119942d66c978f2b4d9af89
2719357 <2719357@collegelacite.ca>
2025-02-08 19:00:26 -0500

    Add files via upload

---
+++ 2719357_EF_4/2719357_EF_4/_719357_EF_4App.swift
@@ -0,0 +1,17 @@
+//
+//  _719357_EF_4App.swift
+//  2719357_EF_4
+//
+//  Created by Jovani on 2025-02-02.
+//
+
+import SwiftUI
+
+@main
+struct _719357_EF_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
